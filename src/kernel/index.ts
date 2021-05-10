e2dab90e7db8454619c651372e86314a2c9624a3
Shachar Itzhaky <corwin.amber@gmail.com>
2021-05-10 23:16:57 +0300

    [bugfix] TypeScript issue.
    Error when running with Webpack's ts-loader's `transpileOnly`.
    Apparently it cannot distinguish types when re-exporting (which does
    make sense).

--- src/kernel/index.ts
+++ src/kernel/index.ts
@@ -1,3 +1,4 @@
 export { ProcessBase as Process, WorkerProcess, BareProcess } from './process';
-export { ExecCore, ExecCoreOptions } from './exec';
+export { ExecCore } from './exec';
+export type { ExecCoreOptions } from './exec';
 export { SharedVolume } from './services/shared-fs';
