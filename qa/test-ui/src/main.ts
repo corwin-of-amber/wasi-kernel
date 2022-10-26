fbd61d1ad718e2e3a8b93a696b27f31b1299ce06
Shachar Itzhaky <corwin.amber@gmail.com>
2022-10-26 13:45:32 +0300

    [qa] [refactor] Move shell stuff.

---
+++ qa/test-ui/src/main.ts
@@ -0,0 +1,119 @@
+import fs from 'fs';
+import * as kernel from '../../../src/kernel';
+import { Volume, MemFSVolume, SharedVolume } from '../../../src/kernel';
+import { PackageManager, Resource } from '../../../src/kernel/services/package-mgr';
+
+import { Shell } from './shell/shell';
+
+
+
+async function runBare(wasm: string, argv: string[], hd: MemFSVolume, termInput: string[] = []) {
+    var p = new kernel.BareProcess(wasm, {argv, tty: true, fs: hd});
+    Object.assign(window, {p});
+
+    p.on('start', () => {
+        for (let ln of termInput) p.stdin.write(ln);
+    });
+    p.stdout.on('data', d => term.data(d));
+}
+
+
+async function runInWorker(wasm: string, argv: string[], hd?: Volume, termInput: string[] = []) {
+    var opts = {workerScriptUri: new URL('worker.js', location.href)},
+        p = new kernel.WorkerProcess(null, {...opts, argv});
+
+    Object.assign(window, {p});
+    
+    if (hd instanceof SharedVolume) {
+        p.worker.postMessage({
+            volume: {storage: hd.storage}
+        });
+    }
+    else {
+        p.worker.postMessage({
+            upload: {
+                '/bin/ls': '#!/bin/coreutils/ls.wasm',
+                '/bin/touch': '#!/bin/coreutils/touch.wasm'
+            }
+        })
+    }
+
+    p.on('start', async () => {
+        console.log('%c[start]', 'color: red');
+        await new Promise(r => setTimeout(r, 500));
+        for (let ln of termInput) {
+            await new Promise(r => setTimeout(r, 500));
+            p.stdin.write(ln);
+        }
+    });
+    p.stdout.on('data', d => term.data(d));
+
+    let shell = new Shell();
+    shell.volume = hd;
+    shell.pool.opts = opts;
+    shell.pool.handleSpawns(p);
+    shell.on('data', d => term.data(d));
+
+    Object.assign(window, {p, shell});
+
+    p.exec(wasm);
+}
+
+
+class Terminal {
+    td = new TextDecoder
+
+    constructor(private el: HTMLElement) {
+    }
+
+    data(d: string | Uint8Array) {
+        if (d instanceof Uint8Array) d = this.td.decode(d);
+        console.log(d);
+        this.el.innerText += d;
+    }
+}
+
+let term: Terminal;
+
+
+async function main() {
+    term = new Terminal(document.getElementById('stdout'));
+
+    //const WASM = '/apps/busy.wasm', ARGV = ['ls', '/home'];
+    const WASM = '/bin/dash.wasm', ARGV = [], TERM_INPUT = [
+        'busy touch /home/ax\n', 'busybox ls -l\n']; //'echo $PATH /bin/*\n', 'cd /home\n', 'ls -l\n', 'touch a\n', 'ls -l .\n', 'ls /\n', 'echo done\n'];
+    //const WASM = '/bin/ocamlrun.wasm', ARGV = ['.', '/usr/local/lib/ocaml/ocaml'], TERM_INPUT = ["8 + 5 ;;\n", "8 * 5 ;;\n"]
+    //const WASM = '/bin/coreutils/ls.wasm', ARGV = ['ls', '-l', '/'], TERM_INPUT = [];
+
+    await kernel.init();
+
+    var raw = new SharedArrayBuffer(64 * (1 << 20));
+
+    var //hd = new MemFSVolume(),
+        hd = new SharedVolume(raw),
+        pm = new PackageManager(hd);
+
+    Object.assign(window, {hd, raw});
+
+    //await pm.installTar('/usr/local/lib/ocaml', new Resource('/bin/ocaml-base.tar'));
+    hd.mkdirSync('/usr/bin', {recursive: true});
+    hd.mkdirSync('/home/x', {recursive: true});
+        
+    await pm.installFile('/bin/ls', "#!/bin/coreutils/ls.wasm");
+    await pm.installFile('/bin/touch', "#!/bin/coreutils/touch.wasm");
+    await pm.installFile('/bin/busy', "#!/apps/busy.wasm");
+    await pm.installFile('/bin/busybox', "#!/bin/busybox.wasm");
+
+    //var shadow = new MemFSVolume(MemFS.with_storage(raw));
+    //Object.assign(window, {shadow});
+
+    //runBare(WASM, ARGV, hd, TERM_INPUT); 
+    runInWorker(WASM, ARGV, hd, TERM_INPUT);
+
+
+    //p.mountFs(volume);
+    //p.stdout.on('data', print);
+}
+
+Object.assign(window, {main});
+document.addEventListener('DOMContentLoaded', main);
---
+++ qa/test-ui/src/shell/shell.ts
@@ -0,0 +1,149 @@
+import { EventEmitter } from 'events';
+
+import { Process, Volume, SharedVolume } from '../../../../src/kernel';
+import { WorkerPool, ProcessLoader, WorkerPoolItem, SpawnArgs } 
+       from '../../../../src/kernel/services/worker-pool';
+
+import path from 'path';
+
+
+
+class Shell extends EventEmitter implements ProcessLoader {
+
+    mainProcess: Process
+    fgProcesses: Process[]
+    pool: WorkerPool
+    env: {[name: string]: string}
+    files = new Map<string, string>()
+    volume: Volume = undefined;
+
+    constructor() {
+        super();
+        this.fgProcesses = [];
+        this.pool = new WorkerPool();
+        this.pool.loader = this;
+        this.pool.on('worker:data', (_, x) => this.emit('data', x));
+        this.env = {PATH: '/bin', HOME: '/home', TERM: 'xterm-256color'};
+        this.volume = undefined;
+    }
+
+    spawn(prog: string, argv: string[], env?: {[name: string]: string}) {
+        if (!path.isAbsolute(prog) && env.PWD)
+            prog = path.join(env.PWD, prog);
+
+        var wasm: string,
+            file = this._getFile(prog),
+            interp = file && this.shebang(file);
+
+        if (interp) {
+            let iargs = interp.ln.split(/\s+/);
+            if (interp.nl) iargs.push(prog);
+            wasm = iargs[0];
+            argv = [argv[0], ...iargs.slice(1), ...argv.slice(1)];
+        }
+        else
+            wasm = prog;
+
+        var p = this.pool.spawn(wasm, argv, env);
+        this.fgProcesses.unshift(p.process);
+
+        p.promise
+            .then(<any>((ev: {code:number}) => console.log(`${prog} - exit ${ev.code}`)))
+            .catch((e: Error) => console.error(`${prog} - error;`, e))
+            .finally(() => this.fgProcesses[0] === p.process 
+                            && this.fgProcesses.shift());
+
+        return p;
+    }
+
+    _getFile(filename: string) {
+        if (Object.hasOwn(this.files, filename)) {
+            return this.files[filename];
+        }
+        try {
+            return this.volume.readFileSync(filename);
+        }
+        catch { return undefined; }
+    }
+
+    populate(p: WorkerPoolItem, spawnArgs: SpawnArgs) {
+        if (this.volume instanceof SharedVolume)
+            p.process.mountFs(this.volume);
+        /*
+        if (!this.filesUploaded) {
+            p.process.worker.postMessage({upload: this.files});
+            this.filesUploaded = true;
+        }
+        */
+        p.process.on('syscall', ev => {
+            if (ev.func === 'ioctl:tty' && ev.data.fd === 0)
+                this.emit('term-ctrl', ev.data.flags);
+        });
+        p.process.opts.proc = {funcTableSz: 16348}; // @todo get size from wasm somehow?
+    }
+
+    exec(p: WorkerPoolItem, spawnArgs: SpawnArgs) {
+        if (spawnArgs.wasm.startsWith('/bin/ocaml')) {  // @todo this is OCaml-specific; just an experiemnt for now
+            var preload = ['dllcamlstr', 'dllunix', 'dllthreads'].map(b => ({
+                name: `${b}.so`, uri: `/bin/ocaml/${b}.wasm`
+            }));
+            
+            p.process.worker.postMessage({dyld: {preload}});
+        }
+
+        this.pool.exec(p, spawnArgs);
+    }
+
+    shebang(script: string | Uint8Array) {
+        var magic = "#!", idx: number, ln: string;
+        if (typeof script == 'string') {
+            if (script.startsWith(magic)) {
+                idx = script.indexOf('\n');
+                ln = (idx > -1) ? script.substring(2, idx)
+                                : script.substring(2);
+            }
+        }
+        else if (script instanceof Uint8Array) {
+            if (script[0] == magic.charCodeAt(0) && script[1] == magic.charCodeAt(1)) {
+                var idx = script.indexOf('\n'.charCodeAt(0));
+                ln = Buffer.from((idx > -1) ? script.subarray(2, idx) 
+                                            : script.subarray(2))
+                      .toString('utf-8');
+            }
+        }
+        return ln ? {ln, nl: idx > -1} : undefined;
+    }
+
+    write(data: string | Uint8Array) {
+        var fgp = this.fgProcesses[0];
+        if (fgp) {
+            if (typeof data === 'string')
+                fgp.stdin.write(data);
+            else
+                fgp.stdin_raw.write(data);
+        }
+    }
+
+    sendEof() {
+        var fgp = this.fgProcesses[0];
+        if (fgp) fgp.stdin.end();
+    }
+
+    /* Some file handling utilities that should not actually be part of the shell */
+
+    readFile(fp: string, enc?: 'utf-8') {
+        return new Blob([this.volume.readFileSync(fp, enc)],
+            {type: enc ? 'text/plain' : 'application/octet-stream'});
+    }
+
+    downloadFile(fp: string, enc?: 'utf-8') {
+        var blob = this.readFile(fp, enc), 
+            a = document.createElement('a');
+        a.setAttribute('href', URL.createObjectURL(blob));
+        a.setAttribute('download', path.basename(fp));
+        a.click();
+    }
+}
+
+
+export { Shell }
